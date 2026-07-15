1dc97d3549bf6a897fec66d5380aa37f3e2d9175
Juipe <juipe@proton.me>
2026-07-15 17:16:54 +0500

    Testing 90

--- bot.py
+++ bot.py
@@ -40,4 +40,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info("stopped by user")
+        logging.info("stopped by user 90")
